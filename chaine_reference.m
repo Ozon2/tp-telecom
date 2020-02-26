f49749132b807b5cce5fefb721ae5e7a3d08f799
Grunig Axel <agrunig@eowyn.enseeiht.fr>
2020-02-26 11:50:09 +0100

    diagramme de l'oeil fait

--- chaine_reference.m
+++ chaine_reference.m
@@ -2,16 +2,18 @@
 close all
 clear all
 
-%% Initialisation des constantes
-Nb = 10; % Nombre de bits
-Ne = 8;    % Nombre d'échantillons par période symbole
+%% Initialisation des coNetantes
+Nb = 100; % Nombre de bits
+Ne = 10;    % Nombre d'échantillone par période symbole
+SPAN = 3;
 h = ones(1, Ne); % Répertoire impulsionnelle du filtre de mise en forme
+% h = rcosdesign (0.5, SPAN, Ne, 'sqrt');
 hr = fliplr(h);  %Filtre de réception adapté
-t0 = Ne;
+t0 = length(h);
 
 %% Génération des bits et Mapping
 bits = randi([0,1],1,Nb);
-symboles = 2*bits - 1;
+symboles = 3*(2*bits - 1);
 
 peigne_dirac = kron(symboles, [1, zeros(1,Ne-1)]);
 x = filter(h, 1, peigne_dirac);
@@ -28,8 +30,9 @@ ze = z(t0:Ne:Ne*Nb);
 %% Décision
 bits_estimes = (ze > 0);
 TEB = sum(bits ~= bits_estimes)/Nb;
+% TEB = sum(bits(1:Nb-SPAN) ~= bits_estimes)/Nb;
 
-%% Tracé de la chaine
+%% Tracé de la chaiNe
 figure;
 plot((1:Nb*Ne)/Ne, peigne_dirac,'LineWidth',2);
 hold on
@@ -38,5 +41,22 @@ plot((1:Nb*Ne)/Ne, z, 'b.-','LineWidth',2);
 plot((t0:Ne:Nb*Ne)/Ne, ze ,'gp','LineWidth',3);
 legend ("Peigne de Dirac", "x(t)", "z(t)", "ze(t)");
 xlabel("Temps (en période T_s)");
-title("chaine de référence");
+title("chaines de référence");
 grid
+
+%% Diagramme de l'oeil
+eyediagram (z(length(h):Nb*Ne), 2*Ne, 2*Ne);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
