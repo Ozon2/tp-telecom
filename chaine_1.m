3c00c9f5bc5b1b77f724edb5a9bd2a051b21e4ca
Grunig Axel <agrunig@daurade.enseeiht.fr>
2020-03-25 08:56:30 +0100

    chaine 1

--- chaine_1.m
+++ chaine_1.m
@@ -3,9 +3,9 @@ close all
 clear all
 
 %% Initialisation des constantes
-Nb = 1000; % Nombre de bits
-Ne = 8;    % Nombre d'échantillon par période symbole
-h = ones(1, Ne); % Répertoire impulsionnelle du filtre de mise en forme
+Nb = 10000; % Nombre de bits
+Ns = 8;    % Nombre d'échantillon par période symbole
+h = ones(1, Ns); % Répertoire impulsionNslle du filtre de mise en forme
 hr = fliplr(h);  %Filtre de réception adapté
 t0 = length(h);
 
@@ -13,16 +13,8 @@ t0 = length(h);
 bits = randi([0,1],1,Nb);
 symboles = 3*(2*bits - 1);
 
-peigne_dirac = kron(symboles, [1, zeros(1,Ne-1)]);
-x = filter(h, 1, peigne_dirac);
-
-%% Densité spectrale de puissance
-X = fft(x);
-DSP = 1/(Nb*Ne) * abs(X).^2;
-figure; semilogy(linspace(-0.5, 0.5, length(DSP)),fftshift(DSP));
-title("DSP du signal transmis");
-xlabel("Fréquence normalisée");
-ylabel("DSP(f)");
+peigNs_dirac = kron(symboles, [1, zeros(1,Ns-1)]);
+x = filter(h, 1, peigNs_dirac);
 
 %% Canal
 r = x;
@@ -30,63 +22,70 @@ r = x;
 %% Reception
 z = filter(hr, 1, r);
 
+
+
+%% Densité spectrale de puissance
+Z = fft(z);
+DSP = 1/(Nb*Ns) * abs(Z).^2;
+figure; semilogy(linspace(-0.5, 0.5, length(DSP)),fftshift(DSP));
+title("DSP du signal transmis");
+xlabel("Fréquence normalisée");
+ylabel("DSP(f)");
+
 %% Echantillonnage
-ze = z(t0:Ne:Ne*Nb);
+ze = z(t0:Ns:Ns*Nb);
 
 %% Tracé de la chaine
 figure;
-plot((1:Nb*Ne)/Ne, peigne_dirac,'LineWidth',2);
+plot((1:Nb*Ns)/Ns, peigNs_dirac,'LineWidth',2);
 hold on
-plot((1:Nb*Ne)/Ne, x, 'r--','LineWidth',2);
-plot((1:Nb*Ne)/Ne, z, 'b.-','LineWidth',2);
-plot((t0:Ne:Nb*Ne)/Ne, ze ,'gp','LineWidth',3);
-legend ("Peigne de Dirac", "x(t)", "z(t)", "ze(t)");
+plot((1:Nb*Ns)/Ns, x, 'r--','LineWidth',2);
+plot((1:Nb*Ns)/Ns, z, 'b.-','LineWidth',2);
+plot((t0:Ns:Nb*Ns)/Ns, ze ,'gp','LineWidth',3);
+legend ("PeigNs de Dirac", "x(t)", "z(t)", "ze(t)");
 xlabel("Temps (en période T_s)");
-title("Chaine de référence");
+title("chaine de référence");
 grid
 
 %% Diagramme de l'oeil
-eyediagram (z(length(h):Nb*Ne), 2*Ne, 2*Ne);
+eyediagram (z(length(h):Nb*Ns), 2*Ns, 2*Ns);
 
 %% Décision
 bits_estimes = (ze > 0);
 TEB = sum(bits ~= bits_estimes)/Nb;
 fprintf("Le TEB sans bruit vaut : %d \n", TEB);
 
-
-
 %% TEB avec bruit
 Eb_sur_N0_dB = linspace(0,6,100);
 Eb_sur_N0 = 10.^(Eb_sur_N0_dB./10);
 TEBs = zeros(1,length(Eb_sur_N0));
 Pr = mean(abs(x).^2);
-sigmas = Pr*Ne./(2*Eb_sur_N0);
+sigmas = Pr*Ns./(2*Eb_sur_N0);
 
-TEB_theorique = 0;
-p = 0.5; % probabilité d'une erreur (TODO)
-Nelimit = Nb*p/(1-p); % Nombre d'erreurs à atteindre
+Nelimite = 100;
 for i = 1:length(sigmas)
-    Nerr = 0; % Nombre d'erreurs
+    Nerr = 0;
     nbEssais = 0;
-    while (Nerr < Nelimit)S
+    while (Nerr < Nelimite)
         % Canal avec bruit AWGN
-        r = x + sqrt(sigmas(i))*randn(1,length(x));
+        r = x + sqrt(sigmas(i))*randn(1,length(x)); 
         % Réception
-        z = filter(hr, 1, r);
-        % Echantillonage
-        ze = z(t0:Ne:Ne*Nb);
+         z = filter(hr, 1, r); 
+        % Echantilonage
+        ze = z(t0:Ns:Ns*Nb); 
         % Décision
         bits_estimes = (ze > 0);
-        NerrActuel =  sum(bits ~= bits_estimes);
+        NerrActuel = sum(bits ~= bits_estimes);
         Nerr = Nerr + NerrActuel;
-        TEBs(i) = TEBs(i) + NerrActuel/Nb;
         nbEssais = nbEssais + 1;
     end
-    TEBs(i) = TEBs(i)/nbEssais;
+    TEBs(i) = Nerr/(nbEssais*Nb);
 end
 
 figure;
-semilogy(Eb_sur_N0_dB,TEBs);
+semilogy(Eb_sur_N0_dB,TEBs); hold on;
+semilogy(Eb_sur_N0_dB,qfunc(sqrt(2*Eb_sur_N0)));
 title("TEB en fonction de (Eb/N0) (dB)");
 xlabel("(Eb/N0) (dB)");
 ylabel("TEB");
+legend("TEB simmulé","TEB théorique");
