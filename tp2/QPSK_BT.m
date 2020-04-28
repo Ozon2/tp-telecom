4ca27f7ed075ecb30d6965bc3d5739000a7652ba
Grunig Axel <agrunig@salameche.enseeiht.fr>
2020-04-28 08:17:51 +0200

    rectification chaine 1

--- tp2/QPSK_BT.m
+++ tp2/QPSK_BT.m
@@ -14,7 +14,7 @@ TESs = zeros(1,length(Eb_sur_N0));          % Initialisation du TES
 Nerr=zeros(1,length(Eb_sur_N0_dB));         % Initialisation du nombre d'erreur
 Nerrlimite = 100;                           % nombre d'erreur limite pour une precision de 10%
 
-Nbits = 100 ;                               % Nombre de bits emis
+Nbits = 100;                               % Nombre de bits emis
 M = 4 ;                                     % Taille de la constellation
 
 Fe=10000;                                   % frequence d'echantillonnage
@@ -24,65 +24,69 @@ retard_Ts=6;                                % Longueur du filtre en SRRCF en nom
 alphaSRRCF=0.35;                            % Coefficient roll off
 h=rcosdesign(alphaSRRCF,retard_Ts,Ns);      % Filtre d'emission
 hr = fliplr(h);                             % Filtre de réception adapte
-t0 = retard_Ts*Ns+1;                        % t0 represente l'instant de prise de décision
-                                            % pour le symbole a0 emis a t=0
 
 fp=2000;                                    % frequence porteuse
 fpN=fp/Fe;                                  % frequence porteuse normalisee
 Sigma2 = zeros(1,length(Eb_sur_N0_dB));     % Initialisation de Sigma2
 nbEssais=zeros(1,length(Sigma2));           % Initialisation du nombre d'essais
-bits_estime = zeros(1,Nbits - 2*retard_Ts); % Initialisation de bits_estime
+bits_estimes = zeros(1,Nbits); % Initialisation de bits_estime
+
+temps = [1:Nbits/log2(M)*Ns];
+
+% Création filtre passe bas
+fc = fp;
+N = 101;
+k = (-(N-1)/2 : (N-1)/2);
+filtre_bas = 2*(fc/Fe)*sinc(2*k*(fc/Fe));
 
 
 % Emission
 bits=randi([0 1],1,Nbits);      % Generation des bits
-symbI=1-2*bits(1:2:Nbits);      % Codage symboles sur la voie I
-symbQ=1-2*bits(2:2:Nbits);      % Codage symboles sur la voie Q
+symbI=2*bits(1:2:Nbits)-1;      % Codage symboles sur la voie I
+symbQ=2*bits(2:2:Nbits)-1;      % Codage symboles sur la voie Q
 symboles = symbI+1i*symbQ;
-peigne=zeros(1,Nbits/log2(M)*Ns);
-peigne(1:Ns:Nbits/log2(M)*Ns)=symboles; % Symboles du peigne de Dirac
-xe=filter(h,1,peigne); % Generation du signal passe-bas
+peigne= kron(symboles, [1, zeros(1,Ns-1)]); % Symboles du peigne de Dirac
+xe=conv(peigne, h, 'same'); % Generation du signal passe-bas
         
 %Transposition sur fréquence porteuse
-x=real(exp(1i*2*pi*fpN*[1:Nbits/log2(M)*Ns]).*xe); % Generation du signal passe-bande
+x=real(exp(1i*2*pi*fpN*temps).*xe); % Generation du signal passe-bande
 
-Pr = mean(abs(x).^2);                           % Calcul de la puissance du signal recu
-Sigma2 = Pr*Ns./(2*log2(M)*Eb_sur_N0);    % Calcul de la variance du bruit
+Pr = mean(abs(x).^2);                       % Calcul de la puissance du signal recu
+Sigma2 = Pr*Ns./(2*log2(M)*Eb_sur_N0);      % Calcul de la variance du bruit
 
 for k=1:length(Eb_sur_N0_dB)
     
-    while (Nerr(k)<Nerrlimite)
+    while (Nerr(k) < Nerrlimite)
         
         
 
         % Canal
-        signal_canal = x + sqrt(Sigma2(k))*randn(1,length(xe)); % Signal du canal
+        signal_canal = x + sqrt(Sigma2(k))*randn(1,length(x)); % Signal du canal
         
         % Retour en Bande de base
-        signal_reel = signal_canal .* cos(2*pi*fpN*[1:Nbits/log2(M)*Ns]); % correspond à x(t)*cos(2*pi*fp*t)
-        signal_complexe = signal_canal .* sin(2*pi*fpN*[1:Nbits/log2(M)*Ns]); % correspond à x(t)*sin(2*pi*fp*t)
+        signal_reel = signal_canal .* cos(2*pi*fpN*temps);     % correspond à x(t)*cos(2*pi*fp*t)
+        signal_complexe = signal_canal .* sin(2*pi*fpN*temps); % correspond à x(t)*sin(2*pi*fp*t)
         
-        signal_recu_reel = filter(hr,1,signal_reel); % Filtrage passe bas de la partie réelle
-        signal_recu_complexe = filter(hr,1,signal_complexe); % Filtrage passe bas de la partie complexe
+        signal_recu_reel = conv(signal_reel,filtre_bas,'same'); % Filtrage passe bas de la partie réelle
+        signal_recu_complexe = conv(signal_complexe,filtre_bas,'same'); % Filtrage passe bas de la partie complexe
         
         % Reception
-        signal_recu = signal_recu_reel - 1i.*signal_recu_complexe; % signal recu
-        z = filter(hr,1,signal_recu); % Signal recu filtré
-        
+        signal_recu = signal_recu_reel - 1i*signal_recu_complexe; % signal recu
+        z = conv(signal_recu, hr, 'same');                        % Signal recu filtré
         % Echantilonage
-        ze = z(t0:Ns:Ns*Nbits/2);
+        ze = z(1:Ns:end);
         
         % Decision
         symboles_estimes = (real(ze) > 0) - (real(ze) < 0) + 1i*((imag(ze) > 0) - (imag(ze) < 0));
         
         % Demapping
         % Les bits impaires correspondent à la partie réelle des symboles
-        bits_estimes(1:2:Nbits - 2*retard_Ts) = real(symboles_estimes);
+        bits_estimes(1:2:end) = real(symboles_estimes) > 0;
         % Les bits paires correspondent à la partie complexe des symboles
-        bits_estimes(2:2:Nbits - 2*retard_Ts) = imag(symboles_estimes);
+        bits_estimes(2:2:end) = imag(symboles_estimes) > 0;
         
         
-        NerrActuel = sum(bits(1:Nbits - 2*retard_Ts) ~= bits_estimes); % Comparaison des bits
+        NerrActuel = sum(bits ~= bits_estimes); % Comparaison des bits
         Nerr(k) = Nerr(k) + NerrActuel; % Calcul du nombre d'erreur
         
         nbEssais(k) = nbEssais(k) + 1; % Calcul du nombre d'essais
@@ -98,15 +102,33 @@ w = window(@blackmanharris, length(x)); % Fenetre du periodogramme
 [Px2,F2] = periodogram(x,w,nfft);       % periodogramme du signal passe bande
 
 figure
+plot(ze,'r+');
+
+figure
+x_r = real(xe);
+subplot(2,1,1); plot(x_r);
+title("Signal en phase");
+x_c = real(xe);
+subplot(2,1,2); plot(x_c);
+title("Signal en quadrature de phase");
+
+figure
+plot(x);
+title("Signal sur fréquence porteuse");
+
+figure
 semilogy(axe_f,fftshift(abs(fft(xe,nfft)).^2))
 hold on
 semilogy(axe_f,fftshift(abs(fft(x,nfft)).^2),'r')
-% semilogy(F1, fftshift(Px1));
-% hold on
-% semilogy(F2, fftshift(Px2), 'r');
 xlabel('frequence normalisee')
 grid
-legend('PSD signal passe-bas','PSD signal passe-bande')
+legend('DSP signal passe-bas','DSP signal passe-bande')
+
+figure
+semilogy(F1, fftshift(Px1));
+hold on
+semilogy(F2, fftshift(Px2), 'r');
+
 
 TEB_theo = qfunc(sqrt(2*Eb_sur_N0));
 Pb = TEB_theo; % Puissance du bruit
